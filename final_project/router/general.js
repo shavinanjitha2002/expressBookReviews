e15e5ed8833aa140ff2a977ae4b12493e3f3c2f2
Shavin Anjitha <shavinanjithachandrawansha@gmail.com>
2023-09-04 09:12:15 +0530

    Add promises ot the general API endpoints

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -23,45 +23,73 @@ public_users.post("/register", (req, res) => {
 
 // Get the book list available in the shop
 public_users.get("/", function (req, res) {
-    res.send(JSON.stringify(books, null, 4));
+    const readBooks = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(JSON.stringify(books, null, 4));
+        }, 1000);
+    });
+
+    readBooks.then((result) => {
+        res.send(result);
+    });
 });
 
 // Get book details based on ISBN
-public_users.get("/isbn/:isbn", function (req, res) {
-    const isbn = req.params.isbn;
-    res.send(JSON.stringify(books[isbn], null, 4));
+public_users.get("/isbn/:isbn", async function (req, res) {
+    const readBook = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            const isbn = req.params.isbn;
+            resolve(JSON.stringify(books[isbn], null, 4));
+        }, 1000);
+    });
+
+    readBook.then((response) => {
+        res.send(response);
+    });
 });
 
 // Get book details based on author
-public_users.get("/author/:author", function (req, res) {
-    let author = req.params.author;
-    const author_names = author.split("-");
-    author = author_names.join(" ");
-    // filter the books based on their authors
-    const filteredBooks = {};
-    for (let isbn in books) {
-        if (books[isbn].author === author) {
-            filteredBooks[isbn] = books[isbn];
+public_users.get("/author/:author", async function (req, res) {
+    const filterBooksByAuthor = new Promise((resolve, reject) => {
+        let author = req.params.author;
+        const author_names = author.split("-");
+        author = author_names.join(" ");
+        // filter the books based on their authors
+        const filteredBooks = {};
+        for (let isbn in books) {
+            if (books[isbn].author === author) {
+                filteredBooks[isbn] = books[isbn];
+            }
         }
-    }
 
-    res.send(JSON.stringify(filteredBooks, null, 4));
+        resolve(JSON.stringify(filteredBooks, null, 4));
+    });
+
+    filterBooksByAuthor.them((response) => {
+        res.send(response);
+    });
 });
 
 // Get all books based on title
 public_users.get("/title/:title", function (req, res) {
-    let title = req.params.title;
-    const title_items = title.split("-");
-    title = title_items.join(" ");
-    // filter the books based on their authors
-    const filteredBooks = {};
-    for (let isbn in books) {
-        if (books[isbn].title === title) {
-            filteredBooks[isbn] = books[isbn];
+    const filterBooksByTitle = new Promise((resolve, reject) => {
+        let title = req.params.title;
+        const title_items = title.split("-");
+        title = title_items.join(" ");
+        // filter the books based on their authors
+        const filteredBooks = {};
+        for (let isbn in books) {
+            if (books[isbn].title === title) {
+                filteredBooks[isbn] = books[isbn];
+            }
         }
-    }
 
-    res.send(JSON.stringify(filteredBooks, null, 4));
+        resolve(JSON.stringify(filteredBooks, null, 4));
+    });
+
+    filterBooksByTitle.then((response) => {
+        res.send(response);
+    });
 });
 
 //  Get book review
