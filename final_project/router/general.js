a47e77017cfdc287d755028bfaae78ed793201de
Shavin Anjitha <shavinanjithachandrawansha@gmail.com>
2023-09-04 09:44:15 +0530

    change the main get endpoint with async/await

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -22,20 +22,14 @@ public_users.post("/register", (req, res) => {
 });
 
 // Get the book list available in the shop
-public_users.get("/", function (req, res) {
-    const readBooks = new Promise((resolve, reject) => {
-        setTimeout(() => {
-            resolve(JSON.stringify(books, null, 4));
-        }, 1000);
-    });
+public_users.get("/", async function (req, res) {
+    const results = await JSON.stringify(books, null, 4);
 
-    readBooks.then((result) => {
-        res.send(result);
-    });
+    res.send(results);
 });
 
 // Get book details based on ISBN
-public_users.get("/isbn/:isbn", async function (req, res) {
+public_users.get("/isbn/:isbn", function (req, res) {
     const readBook = new Promise((resolve, reject) => {
         setTimeout(() => {
             const isbn = req.params.isbn;
@@ -49,7 +43,7 @@ public_users.get("/isbn/:isbn", async function (req, res) {
 });
 
 // Get book details based on author
-public_users.get("/author/:author", async function (req, res) {
+public_users.get("/author/:author", function (req, res) {
     const filterBooksByAuthor = new Promise((resolve, reject) => {
         let author = req.params.author;
         const author_names = author.split("-");
